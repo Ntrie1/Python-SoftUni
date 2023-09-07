571ecb1186b413a194f2d5d0a9c347e2565e317a
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-09-07 15:36:58 +0300

    Add files via upload

---
+++ Conditional_Statements_Advanced/exercises/01-Cinema.py
@@ -0,0 +1,18 @@
+screening_type = input()
+rows_count = int(input())
+columns_count = int(input())
+
+income = 0
+
+cinema_capacity = rows_count * columns_count
+
+if screening_type == 'Premiere':
+    income = cinema_capacity * 12.00
+
+elif screening_type == 'Normal':
+    income = cinema_capacity * 7.50
+
+elif screening_type == 'Discount':
+    income = cinema_capacity * 5.00
+
+print(f'{income:.2f}')
